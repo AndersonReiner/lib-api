79ca299c4fa95eacea9b8318c2d1a5d385b1c5cc
Anderson Reiner <reiner.academico@gmail.com>
2025-04-10 08:00:40 -0400

    build: adicionado dependencias: springdoc, postgresql, datajpa, devtools, lombok, validation e dotenv. configurado variaveis de ambiente de confg do banco de dados.

--- src/main/java/com/anderson/lib_api/LibApiApplication.java
+++ src/main/java/com/anderson/lib_api/LibApiApplication.java
@@ -3,10 +3,13 @@ package com.anderson.lib_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.anderson.lib_api.configuration.EnvLoader;
+
 @SpringBootApplication
 public class LibApiApplication {
 
 	public static void main(String[] args) {
+		EnvLoader.load();
 		SpringApplication.run(LibApiApplication.class, args);
 	}
 
